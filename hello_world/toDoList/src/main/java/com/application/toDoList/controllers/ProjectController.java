4cf09b2bbfb524ff427f0f96941efd9f07d7d76b
Lyubava0382 <lubovkrivencova@gmail.com>
2023-12-15 04:22:55 +0300

    Services added

--- hello_world/toDoList/src/main/java/com/application/toDoList/controllers/ProjectController.java
+++ hello_world/toDoList/src/main/java/com/application/toDoList/controllers/ProjectController.java
@@ -5,10 +5,10 @@ import com.application.toDoList.domains.Project;
 import com.application.toDoList.domains.Task;
 import com.application.toDoList.dto.ProjectDTO;
 import com.application.toDoList.enums.ProjectStatus;
-import com.application.toDoList.repositories.PersonRepository;
 import com.application.toDoList.repositories.ProjectRepository;
-import com.application.toDoList.repositories.TaskRepository;
+import com.application.toDoList.services.ProjectService;
 import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,135 +19,48 @@ import javax.validation.Valid;
 @RequestMapping("/project")
 public class ProjectController {
     private final ProjectRepository projectRepository;
-    private final TaskRepository taskRepository;
-    private final PersonRepository personRepository;
+    private final ProjectService projectService;
 
-    public ProjectController(ProjectRepository projectRepository, TaskRepository taskRepository, PersonRepository personRepository) {
+    @Autowired
+    public ProjectController(ProjectRepository projectRepository,  ProjectService projectService) {
         this.projectRepository = projectRepository;
-        this.taskRepository = taskRepository;
-        this.personRepository = personRepository;
+        this.projectService = projectService;
     }
 
     @PostMapping("/admin/create")
     public Project createProject(@RequestBody @Valid ProjectDTO projectDTO) {
-
-        if (projectRepository.findByName(projectDTO.getName()).isPresent()) {
-            // ИСКЛЮЧЕНИЕ ЧТО УЖЕ СУЩЕСТВУЕТ
-            return new Project();
-        }
-
-        Project project = new Project();
-        project.setName(projectDTO.getName());
-        project.setStatus(Enum.valueOf(ProjectStatus.class, projectDTO.getStatus()));
-        // postService.save(convertToPost(postDTO, 0));
-        return projectRepository.save(project);
+        return projectService.create(projectDTO);
     }
 
     @PatchMapping("/admin/{project_id}")
     public Project changeProject(@RequestBody @Valid ProjectDTO projectDTO,
                                  @PathVariable("project_id") ObjectId project_id) {
-        Project project = new Project();
-        if (projectRepository.findById(project_id).isPresent()){
-            project = projectRepository.findById(project_id).get();
-        }
-        else {
-            // исключение проект не найден
-        }
-        if (projectRepository.findByName(projectDTO.getName()).isEmpty()){
-            project.setName(projectDTO.getName());
-        }
-        else{
-            // исключение проект с таким названием уже есть
-        }
-        project.setStatus(Enum.valueOf(ProjectStatus.class, projectDTO.getStatus()));
-        return projectRepository.save(project);
+        return projectService.change(projectDTO, project_id);
     }
 
     @PostMapping("/admin/{project_id}/task/{task_id}")
     public Task taskToProject(@PathVariable("project_id") ObjectId project_id,
                                  @PathVariable("task_id") ObjectId task_id) {
-        Project project = new Project();
-        if (projectRepository.findById(project_id).isPresent()) {
-            project = projectRepository.findById(project_id).get();
-        }
-        else {
-            // ИСКЛЮЧЕНИЕ ПРОЕКТА НЕ СУЩЕСТВУЕТ
-        }
-        Task task = new Task();
-        if (taskRepository.findById(task_id).isPresent()) {
-            task = taskRepository.findById(task_id).get();
-        }
-        else {
-            // ИСКЛЮЧЕНИЕ ЗАДАЧИ НЕ СУЩЕСТВУЕТ
-        }
-        project.getTasks().add(task);
-        task.setProject(project);
-
-        return task; //taskDTO
+        return projectService.addTask(project_id, task_id); //taskDTO
     }
 
     @DeleteMapping("/admin/{project_id}/task/{task_id}")
     public ResponseEntity<HttpStatus> taskOutProject(@PathVariable("project_id") ObjectId project_id,
                                                      @PathVariable("task_id") ObjectId task_id) {
-        Project project = new Project();
-        if (projectRepository.findById(project_id).isPresent()) {
-            project = projectRepository.findById(project_id).get();
-        }
-        else {
-            // ИСКЛЮЧЕНИЕ ПРОЕКТА НЕ СУЩЕСТВУЕТ
-        }
-        Task task = new Task();
-        if (taskRepository.findById(task_id).isPresent()) {
-            task = taskRepository.findById(task_id).get();
-        }
-        else {
-            // ИСКЛЮЧЕНИЕ ЗАДАЧИ НЕ СУЩЕСТВУЕТ
-        }
-        project.getTasks().remove(task);
+        projectService.deleteTask(project_id, task_id);
         return ResponseEntity.ok(HttpStatus.OK);
     }
 
     @PostMapping("/admin/{project_id}/person/{person_id}")
     public Person personToProject(@PathVariable("project_id") ObjectId project_id,
                                 @PathVariable("person_id") ObjectId person_id) {
-        Project project = new Project();
-        if (projectRepository.findById(project_id).isPresent()) {
-            project = projectRepository.findById(project_id).get();
-        }
-        else {
-            // ИСКЛЮЧЕНИЕ ПРОЕКТА НЕ СУЩЕСТВУЕТ
-        }
-        Person person = new Person();
-        if (personRepository.findById(person_id).isPresent()) {
-            person = personRepository.findById(person_id).get();
-        }
-        else {
-            // ИСКЛЮЧЕНИЕ ЗАДАЧИ НЕ СУЩЕСТВУЕТ
-        }
-        project.getExecutors().add(person);
-        person.getProjects().add(project);
-
-        return person;
+        return projectService.addPerson(project_id, person_id);
     }
 
     @DeleteMapping("/admin/{project_id}/person/{person_id}")
     public ResponseEntity<HttpStatus> PersonOutProject(@PathVariable("project_id") ObjectId project_id,
                                                        @PathVariable("person_id") ObjectId person_id) {
-        Project project = new Project();
-        if (projectRepository.findById(project_id).isPresent()) {
-            project = projectRepository.findById(project_id).get();
-        }
-        else {
-            // ИСКЛЮЧЕНИЕ ПРОЕКТА НЕ СУЩЕСТВУЕТ
-        }
-        Person person = new Person();
-        if (personRepository.findById(person_id).isPresent()) {
-            person = personRepository.findById(person_id).get();
-        }
-        else {
-            // ИСКЛЮЧЕНИЕ ЗАДАЧИ НЕ СУЩЕСТВУЕТ
-        }
-        project.getExecutors().remove(person);
+        projectService.deletePerson(project_id, person_id);
         return ResponseEntity.ok(HttpStatus.OK);
     }
 
@@ -169,7 +82,5 @@ public class ProjectController {
         return new Project(new ObjectId(projectDTO.getId()), projectDTO.getName(), ProjectStatus.valueOf(projectDTO.getStatus()));
     }
 
-
-    //Разделить интерфейс
     //Исключения
 }
