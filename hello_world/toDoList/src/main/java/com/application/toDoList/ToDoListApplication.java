2f86b310ec544cd1738f6d9430181a6e96f34902
zdobnovakseniya <kzdobnova2001@gmail.com>
2023-12-16 22:55:32 +0300

    Добавлено создание тасок и подтасок, получение всех тасок

--- hello_world/toDoList/src/main/java/com/application/toDoList/ToDoListApplication.java
+++ hello_world/toDoList/src/main/java/com/application/toDoList/ToDoListApplication.java
@@ -1,7 +1,10 @@
 package com.application.toDoList;
 
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ToDoListApplication {
@@ -9,5 +12,12 @@ public class ToDoListApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ToDoListApplication.class, args);
 	}
+
+//	@Bean
+//	CommandLineRunner run(TaskServiceImpl taskService) {
+//		return args -> {
+////			Task Task1 = new Task("");
+//		};
+//	}
 
 }
